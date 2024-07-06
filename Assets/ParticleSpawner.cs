8d928c175237a2658c8012d5c7ceca52db60aa38
Toma Takamatsu <takamatsutoma@gmail.com>
2024-07-06 03:35:52 -0400

    calculating density and pressure force

--- Assets/ParticleSpawner.cs
+++ Assets/ParticleSpawner.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Mathematics;
 using UnityEngine.UI;
+using System;
 
 public class ParticleSpawner : MonoBehaviour
 {
@@ -18,6 +19,9 @@ public class ParticleSpawner : MonoBehaviour
     public float Gravity = 0;
 
     public float SmoothingRadius = 1;
+    public float Mass = 1;
+    public float TargetDensity = 1;
+    public float PressureMultiplier = 1;
 
     public float BorderWidth = 30;
     public float BorderHeight = 14;
@@ -32,6 +36,8 @@ public class ParticleSpawner : MonoBehaviour
     Vector3[] ParticlePositions;
     Vector3[] ParticleVelocities;
     Vector3[] ParticleForces;
+    float[] ParticleDensities;
+    float[] ParticleProperties;
 
     private void Start()
     {
@@ -42,6 +48,8 @@ public class ParticleSpawner : MonoBehaviour
         ParticlePositions = new Vector3[NumOfParticles];
         ParticleVelocities = new Vector3[NumOfParticles];
         ParticleForces = new Vector3[NumOfParticles];
+        ParticleDensities = new float[NumOfParticles];
+        ParticleProperties = new float[NumOfParticles];
 
         int particlesInRow = (int)math.sqrt(NumOfParticles);
         int particlesInCol = (NumOfParticles - 1) / particlesInRow + 1;
@@ -58,11 +66,12 @@ public class ParticleSpawner : MonoBehaviour
 
     private void Update()
     {
+        CalculateForces();
         for (int i = 0; i < NumOfParticles; i++)
         {
+            ParticleVelocities[i] += (ParticleForces[i] / ParticleDensities[i] + Vector3.down * Gravity)* Time.deltaTime;
             ResolveCollision(i);
-            CalculateForces(i);
-            ParticleVelocities[i] += (ParticleForces[i] + Vector3.down * Gravity)* Time.deltaTime;
+            Debug.LogError(ParticleVelocities[i]);
             Particles[i].transform.position += ParticleVelocities[i] * Time.deltaTime;
             ParticlePositions[i] = Particles[i].transform.position;
         }
@@ -107,16 +116,93 @@ public class ParticleSpawner : MonoBehaviour
         }
     }
 
-    private void CalculateForces(int index)
+    private void CalculateForces()
     {
-
+        CalculateParticleDensities();
+        for (int i = 0; i < NumOfParticles; i++)
+        {
+            ParticleForces[i] = GetPressureForce(i);
+        }
     }
 
-    private float GetSmoothingRadius(float radius, float dist)
-    {
+    private float GetSmoothingRadius(float radius, float dist) {
+        float volume = math.PI * math.pow(radius, 8) / 4;
         float value = math.max(0, radius - dist);
-        return value * value * value;
+        return value * value * value / volume;
+    }
 
+    private float GetSmoothingDerivative(float radius, float dist)
+    {
+        if (dist >= radius) return 0;
+        float f = radius - dist;
+        float scale = -24 / (math.PI * math.pow(radius, 8));
+        return scale * dist * f * f;
+    }
+
+    private float GetDensity(Vector3 point)
+    {
+        float density = 0;
+
+        for (int i = 0; i < NumOfParticles; i++)
+        {
+            float distance = (ParticlePositions[i] - point).magnitude;
+            float influence = GetSmoothingRadius(SmoothingRadius, distance);
+            density += Mass * influence;
+        }
+        return density;
+    }
+
+    private Vector3 GetPressureForce(int index)
+    {
+        Vector3 pressureForce = Vector3.zero;
+
+        for (int i = 0; i < NumOfParticles; i++)
+        {
+            if (i == index) continue;
+
+            Vector3 offset = ParticlePositions[i] - ParticlePositions[index];
+            float dist = offset.magnitude;
+            Vector3 direction = dist == 0 ? GetRandomDirection() : offset / dist;
+            float slope = GetSmoothingDerivative(SmoothingRadius, dist);
+            float density = GetDensity(ParticlePositions[i]);
+            pressureForce += Mass * -DensityToPressure(density) * slope * direction / density;
+        }
+        return pressureForce;
+    }
+
+    private void CalculateParticleProperties()
+    {
+        for (int i = 0; i < NumOfParticles; i++)
+        {
+            ParticleProperties[i] = RandFunction(ParticlePositions[i]);
+        }
+    }
+
+    private void CalculateParticleDensities()
+    {
+        for (int i = 0; i < NumOfParticles; i++)
+        {
+            ParticleDensities[i] = GetDensity(ParticlePositions[i]);
+        }
+    }
+
+    private float DensityToPressure(float density)
+    {
+        float error = density - TargetDensity;
+        return error * PressureMultiplier;
+    }
+
+    private float RandFunction(Vector3 point)
+    {
+        return math.cos(point.y - 3 + math.sin(point.x));
+    }
+
+    private Vector3 GetRandomDirection()
+    {
+        Unity.Mathematics.Random rand = new Unity.Mathematics.Random();
+        float x = rand.NextFloat(-1, 1);
+        float y = rand.NextFloat(-1, 1);
+        return new Vector3(x, y, 0);
     }
 
     private void DrawBorder()
